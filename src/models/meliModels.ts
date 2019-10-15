09ce4cca02b3a5c11883ec89bdd3830c49517e65
Ezequiel Moreno <eze.060@gmail.com>
2019-10-14 23:30:17 -0300

    update to querry stringauth

--- src/models/meliModels.ts
+++ src/models/meliModels.ts
@@ -1,4 +1,4 @@
-import * as mongoose from 'mongoose'
+import mongoose from "mongoose"
 Object.defineProperty(exports, "__esModule", { value: true });
 const Schema = mongoose.Schema
 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,17 +1,18 @@
 import { Request, Response, Express } from "express"
 import { MercadolibreController } from '../controllers/meliController'
+import { WooCommerceController } from '../controllers/woocomerceController'
 import * as path from 'path'
 
 
 export class Routes {
-    public mercadolibreCrontroller: MercadolibreController = new MercadolibreController();
-    
+    public mercadolibreController: MercadolibreController = new MercadolibreController();
+    public WooComerceController: WooCommerceController = new WooCommerceController();
+
     public routes(app:Express) : void {
         let rootDir:string[] = path.dirname(__filename).split(path.sep)
         rootDir.pop()
         rootDir.push('views')
         const viewPath:string =  rootDir.join('/')
-
 
         app.route('/')
         .get((req: Request, res: Response) => {
@@ -20,16 +21,18 @@ export class Routes {
         })
 
         app.route('/ml/login')
-        .get(this.mercadolibreCrontroller.meliAutorize)
+        .get(this.mercadolibreController.meliAutorize)
        
         app.route('/ml/get/categories')
-        .get(this.mercadolibreCrontroller.meliGetProductCategories)
+        .get(this.mercadolibreController.meliGetProductCategories)
 
         app.route('/ml/get/users')
-        .get(this.mercadolibreCrontroller.meliGetUsersByID)
+        .get(this.mercadolibreController.meliGetUsersByID)
         
         app.route('/ml/get/my-products')
-        .get(this.mercadolibreCrontroller.meliGetAllUserProducts)
+        .get(this.mercadolibreController.meliGetAllUserProducts)
 
+        app.route('/woo/products')
+        .get(this.WooComerceController.wooGetProducts)
     }
 }
